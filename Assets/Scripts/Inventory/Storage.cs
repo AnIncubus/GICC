da684a67a74d66b33a8818935e92d715f50d90b8
AnIncubus <incubusthedev@gmail.com>
2019-11-17 03:59:32 -0700

    initial project commit

---
+++ Assets/Scripts/Inventory/Storage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Storage : MonoBehaviour{
+
+    [SerializeField] TextMeshProUGUI woodText;
+    [SerializeField] TextMeshProUGUI ironText;
+    [SerializeField] TextMeshProUGUI foodText;
+    [SerializeField] TextMeshProUGUI toolText;
+    [SerializeField] TextMeshProUGUI goldText;
+
+    int woodResource = 0;
+    int ïronResource = 0;
+    int foodResource = 0;
+    int toolResource = 0;
+    int goldResource = 0;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        woodText.text = woodResource.ToString();
+        woodText.text = woodResource.ToString();
+        woodText.text = woodResource.ToString();
+        woodText.text = woodResource.ToString();
+        woodText.text = woodResource.ToString();
+    }
+}
